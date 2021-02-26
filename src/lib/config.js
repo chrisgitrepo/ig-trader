41adadec8a99965bc40a04383263e5cd27a4edcf
Chris Sarmiento <chrissarmiento@hotmail.com>
2021-02-26 09:26:55 +0000

    update maxSpreads

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,196 +4,196 @@ const markets = [{
   instrumentName: 'GBP/USD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.0,
+  maxSpread: 1.2,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.USDJPY.TODAY.IP',
   instrumentName: 'USD/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 0.8,
+  maxSpread: 1,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURCHF.TODAY.IP',
   instrumentName: 'EUR/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURGBP.TODAY.IP',
   instrumentName: 'EUR/GBP',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.0,
+  maxSpread: 1.2,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURJPY.TODAY.IP',
   instrumentName: 'EUR/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.6,
+  maxSpread: 1.8,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURUSD.TODAY.IP',
   instrumentName: 'EUR/USD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 0.7,
+  maxSpread: 1,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.GBPJPY.TODAY.IP',
   instrumentName: 'GBP/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.6,
+  maxSpread: 2.8,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.USDCHF.TODAY.IP',
   instrumentName: 'USD/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.AUDJPY.TODAY.IP',
   instrumentName: 'AUD/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.4,
+  maxSpread: 1.6,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.AUDUSD.TODAY.IP',
   instrumentName: 'AUD/USD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 0.7,
+  maxSpread: 1,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.CHFJPY.TODAY.IP',
   instrumentName: 'CHF/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.USDCAD.TODAY.IP',
   instrumentName: 'USD/CAD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.8,
+  maxSpread: 2,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURAUD.TODAY.IP',
   instrumentName: 'EUR/AUD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 1.9,
+  maxSpread: 2.1,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.GBPAUD.TODAY.IP',
   instrumentName: 'GBP/AUD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.0,
+  maxSpread: 2.2,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.GBPCHF.TODAY.IP',
   instrumentName: 'GBP/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.1,
+  maxSpread: 3.3,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.CADJPY.TODAY.IP',
   instrumentName: 'CAD/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.6,
+  maxSpread: 2.8,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.EURCAD.TODAY.IP',
   instrumentName: 'EUR/CAD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.1,
+  maxSpread: 3.3,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.GBPCAD.TODAY.IP',
   instrumentName: 'GBP/CAD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.6,
+  maxSpread: 3.8,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.AUDCAD.TODAY.IP',
   instrumentName: 'AUD/CAD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.EURNZD.TODAY.IP',
   instrumentName: 'EUR/NZD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.1,
+  maxSpread: 3.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.AUDNZD.TODAY.IP',
   instrumentName: 'AUD/NZD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.1,
+  maxSpread: 3.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.NZDUSD.TODAY.IP',
   instrumentName: 'NZD/USD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.NZDJPY.TODAY.IP',
   instrumentName: 'NZD/JPY',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.6,
+  maxSpread: 2.8,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.AUDCHF.TODAY.IP',
   instrumentName: 'AUD/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.1,
+  maxSpread: 2.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.CADCHF.TODAY.IP',
   instrumentName: 'CAD/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 2.6,
+  maxSpread: 2.8,
   retailMarginPct: 0.033
 }, {
   epic: 'CS.D.NZDCAD.TODAY.IP',
   instrumentName: 'NZD/CAD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 3.6,
+  maxSpread: 3.8,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.GBPNZD.TODAY.IP',
   instrumentName: 'GBP/NZD',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 5.1,
+  maxSpread: 5.3,
   retailMarginPct: 0.05
 }, {
   epic: 'CS.D.NZDCHF.TODAY.IP',
   instrumentName: 'NZD/CHF',
   instrumentType: 'CURRENCIES',
   expiry: 'DFB',
-  maxSpread: 4.1,
+  maxSpread: 4.3,
   retailMarginPct: 0.05
 }
 // {
