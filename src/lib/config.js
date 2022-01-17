5251e52c4d54cfac0977ad7e6f97faf574b90779
Chris Sarmiento <chrissarmiento@hotmail.com>
2022-01-17 12:41:23 +0000

    Revert "Remove CHF"
    This reverts commit 1fd1435a41e4a76e42de3f8faaf74280cda11d68.

--- src/lib/config.js
+++ src/lib/config.js
@@ -14,6 +14,13 @@ const markets = [{
   maxSpread: 1,
   retailMarginPct: 0.033
 }, {
+  epic: 'CS.D.EURCHF.TODAY.IP',
+  instrumentName: 'EUR/CHF',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 2.3,
+  retailMarginPct: 0.033
+}, {
   epic: 'CS.D.EURGBP.TODAY.IP',
   instrumentName: 'EUR/GBP',
   instrumentType: 'CURRENCIES',
@@ -42,6 +49,13 @@ const markets = [{
   maxSpread: 2.8,
   retailMarginPct: 0.033
 }, {
+  epic: 'CS.D.USDCHF.TODAY.IP',
+  instrumentName: 'USD/CHF',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 2.3,
+  retailMarginPct: 0.033
+}, {
   epic: 'CS.D.AUDJPY.TODAY.IP',
   instrumentName: 'AUD/JPY',
   instrumentType: 'CURRENCIES',
@@ -55,6 +69,13 @@ const markets = [{
   expiry: 'DFB',
   maxSpread: 1,
   retailMarginPct: 0.05
+}, {
+  epic: 'CS.D.CHFJPY.TODAY.IP',
+  instrumentName: 'CHF/JPY',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 2.3,
+  retailMarginPct: 0.033
 }, {
   epic: 'CS.D.USDCAD.TODAY.IP',
   instrumentName: 'USD/CAD',
@@ -76,6 +97,13 @@ const markets = [{
   expiry: 'DFB',
   maxSpread: 2.2,
   retailMarginPct: 0.05
+}, {
+  epic: 'CS.D.GBPCHF.TODAY.IP',
+  instrumentName: 'GBP/CHF',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 3.3,
+  retailMarginPct: 0.033
 }, {
   epic: 'CS.D.CADJPY.TODAY.IP',
   instrumentName: 'CAD/JPY',
@@ -104,6 +132,102 @@ const markets = [{
   expiry: 'DFB',
   maxSpread: 2.3,
   retailMarginPct: 0.05
-}]
+},
+// {
+//   epic: 'CS.D.EURNZD.TODAY.IP',
+//   instrumentName: 'EUR/NZD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 3.3,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.AUDNZD.TODAY.IP',
+//   instrumentName: 'AUD/NZD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 3.3,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.NZDUSD.TODAY.IP',
+//   instrumentName: 'NZD/USD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 2.3,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.NZDJPY.TODAY.IP',
+//   instrumentName: 'NZD/JPY',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 2.8,
+//   retailMarginPct: 0.05
+// },
+{
+  epic: 'CS.D.AUDCHF.TODAY.IP',
+  instrumentName: 'AUD/CHF',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 2.3,
+  retailMarginPct: 0.05
+}, {
+  epic: 'CS.D.CADCHF.TODAY.IP',
+  instrumentName: 'CAD/CHF',
+  instrumentType: 'CURRENCIES',
+  expiry: 'DFB',
+  maxSpread: 2.8,
+  retailMarginPct: 0.033
+},
+// {
+//   epic: 'CS.D.NZDCAD.TODAY.IP',
+//   instrumentName: 'NZD/CAD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 3.8,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.GBPNZD.TODAY.IP',
+//   instrumentName: 'GBP/NZD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 5.3,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.NZDCHF.TODAY.IP',
+//   instrumentName: 'NZD/CHF',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 4.3,
+//   retailMarginPct: 0.05
+// },
+// {
+//   epic: 'CS.D.USDSGD.TODAY.IP',
+//   instrumentName: 'USD/SGD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 3.1,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.GBPSGD.TODAY.IP',
+//   instrumentName: 'GBP/SGD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 5.1,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.SGDJPY.TODAY.IP',
+//   instrumentName: 'SGD/JPY',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 4.1,
+//   retailMarginPct: 0.05
+// }, {
+//   epic: 'CS.D.EURSGD.TODAY.IP',
+//   instrumentName: 'EUR/SGD',
+//   instrumentType: 'CURRENCIES',
+//   expiry: 'DFB',
+//   maxSpread: 4.1,
+//   retailMarginPct: 0.05
+// }
+]
 
 module.exports = { timeframes, markets }
